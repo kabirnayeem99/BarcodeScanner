4ee230500fda27781f147356c1c896566a22318d
Naimul Kabir <kabirnayeem.99@gmail.com>
2024-10-20 16:52:49 +0600

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Naimul Kabir on 20/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
